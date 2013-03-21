fd25266dfe2fa8bdc5e00560720e633243887b37
Girish Shilamkar <girish.shilamkar@gmail.com>
2013-03-20 21:29:10 -0400

    Merge changes to marvin framework from ipclearance branch
    - Support for keypair authentication in remoteSSHClient
    - Removed hypervisor config dependency from test suites
    - Bug fix in asyncJobMgr to support passing UUIDs to db query
    - Bug fix MySQL UnicodeDecode Error in dbConnection

--- tools/marvin/marvin/cloudstackTestClient.py
+++ tools/marvin/marvin/cloudstackTestClient.py
@@ -24,8 +24,8 @@ import string
 import hashlib
 
 class cloudstackTestClient(object):
-    def __init__(self, mgtSvr=None, port=8096, apiKey = None, securityKey = None, asyncTimeout=3600, defaultWorkerThreads=10, logging=None):
-        self.connection = cloudstackConnection.cloudConnection(mgtSvr, port, apiKey, securityKey, asyncTimeout, logging)
+    def __init__(self, mgtSvr=None, port=8096, user=None, passwd=None, apiKey = None, securityKey = None, asyncTimeout=3600, defaultWorkerThreads=10, logging=None):
+        self.connection = cloudstackConnection.cloudConnection(mgtSvr, port, user, passwd, apiKey, securityKey, asyncTimeout, logging)
         self.apiClient = cloudstackAPIClient.CloudStackAPIClient(self.connection)
         self.dbConnection = None
         self.asyncJobMgr = None
@@ -111,7 +111,7 @@ class cloudstackTestClient(object):
             apiKey = registerUserRes.apikey
             securityKey = registerUserRes.secretkey
         
-        newUserConnection = cloudstackConnection.cloudConnection(self.connection.mgtSvr, self.connection.port, apiKey, securityKey, self.connection.asyncTimeout, self.connection.logging)
+        newUserConnection = cloudstackConnection.cloudConnection(self.connection.mgtSvr, self.connection.port, self.connection.user, self.connection.passwd, apiKey, securityKey, self.connection.asyncTimeout, self.connection.logging)
         self.userApiClient = cloudstackAPIClient.CloudStackAPIClient(newUserConnection)
         self.userApiClient.connection = newUserConnection
         return self.userApiClient
--- tools/marvin/marvin/deployDataCenter.py
+++ tools/marvin/marvin/deployDataCenter.py
@@ -387,6 +387,7 @@ class deployDataCenters():
 
         self.testClient = \
         cloudstackTestClient.cloudstackTestClient(mgt.mgtSvrIp, mgt.port, \
+                                                  mgt.user, mgt.passwd, \
                                                   mgt.apiKey, \
                                                   mgt.securityKey, \
                                             logging=self.testClientLogger)
@@ -395,6 +396,7 @@ class deployDataCenters():
             self.testClient.close()
             self.testClient = \
             cloudstackTestClient.cloudstackTestClient(mgt.mgtSvrIp, 8080, \
+                                                      mgt.user, mgt.passwd, \
                                                       apiKey, securityKey, \
                                              logging=self.testClientLogger)
 
@@ -405,6 +407,11 @@ class deployDataCenters():
                                       dbSvr.passwd, dbSvr.db)
 
         self.apiClient = self.testClient.getApiClient()
+        """set hypervisor"""
+        if mgt.hypervisor:
+            self.apiClient.hypervisor = mgt.hypervisor
+        else:
+            self.apiClient.hypervisor = "XenServer"     #Defaults to Xenserver
 
     def updateConfiguration(self, globalCfg):
         if globalCfg is None:
